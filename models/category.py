1650b1578736f3cadd70f993668cd1ac03c5698c
Gustavo Sobreiro de Assis <gustavo.sobreiro@olist.com>
2021-01-28 13:41:35 -0300

    Category models test pytest code

---
+++ models/category.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, String
+from models.base_model import BaseModel
+from sqlalchemy.orm import validates
+
+
+class Category(BaseModel):
+    __tablename__ = 'category'
+    name = Column(String(length=150))
+    description = Column(String(length=200))
+
+    def __init__(self, name: str, description: str) -> None:
+        self.name = name
+        self.description = description
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if name is None:
+            raise ValueError("Please, write a name!")
+        elif name.strip(' ') == '':
+            raise ValueError("Name can't be null!")
+        return name
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if description is None:
+            raise ValueError("Please, write a description!")
+        elif description.strip(' ') == '':
+            raise ValueError("Description can't be null!")
+        return description
---
+++ tests/models/category.py
@@ -0,0 +1,40 @@
+from backend.models.base_model import BaseModel
+from models.category import Category
+
+name = 'Category'
+description = 'Description'
+
+category = Category(name, description)
+
+
+def test_compare_column_model():
+    assert category.name == name
+    assert category.description == description
+
+
+def test_compare_type_column_model():
+    assert type(category.name) == str
+    assert type(category.description) == str
+
+
+def test_compare_isinstance():
+    assert isinstance(category, BaseModel)
+    assert isinstance(category, Category)
+
+
+def test_has_attribute():
+    assert hasattr(category, 'name')
+    assert hasattr(category, 'description')
+
+def test_validate_name():
+    try:
+        Category(None, 'Eletronicos')
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
+
+def test_validate_description():
+    try:
+        Category('Moveis', '     ')
+    except Exception as e:
+        assert isinstance(e, ValueError)
