7d5203bacd040c18f4db89376d180fb08fb4a690
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-29 16:55:51 -0300

    Added new functions to test all exceptions from category

--- tests/models/test_category.py
+++ tests/models/test_category.py
@@ -28,15 +28,30 @@ def test_has_attribute():
     assert hasattr(category, 'name')
     assert hasattr(category, 'description')
 
-def test_validate_name():
+
+def test_validate_name_none():
     try:
         Category(None, 'Eletronicos')
     except Exception as e:
         assert isinstance(e, ValueError)
 
 
-def test_validate_description():
+def test_validate_name_empty():
     try:
-        Category('Moveis', '     ')
+        Category(' ', 'Eletronicos')
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
+
+def test_validate_description_none():
+    try:
+        Category('Moveis', None)
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
+
+def test_validate_description_empty():
+    try:
+        Category('Moveis', ' ')
     except Exception as e:
         assert isinstance(e, ValueError)
