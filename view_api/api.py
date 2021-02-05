f2006649e9e068e8137b57b6930417f6a965d001
Gustavo Sobreiro de Assis <gustavo.sobreiro@olist.com>
2021-02-05 11:16:00 -0300

    Create api for category.

---
+++ view_api/api.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('.')
+from flask import Flask
+from flask_restful import Api
+
+from resources.category_resource import CategoryResource
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(CategoryResource, '/api/category', endpoint='categorys')
+api.add_resource(CategoryResource, '/api/category/<int:id>', endpoint='category')
+
+
+# @app.route('/')
+# def index():
+#     return 'Bem Vindo!'
+
+
+app.run(debug=True)
